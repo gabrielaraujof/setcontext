b59c4b741030de4cd716279dad4ac23fb3b328b9
Gabriel Araujo <gabrielaraujof@outlook.com.br>
2015-06-03 14:33:53 -0300

    Source initial commit

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/setcontext/"
+http_images_path = "/setcontext/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/setcontext/fonts"
+css_dir = "public/setcontext/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
